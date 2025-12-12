2fcbed887f4e5f1f056b1c6d6ab992e09860d156
omirambe361 <omirambe361@gmail.com>
2025-12-12 12:07:50 +0200

    last push for railway update again

--- mysite/settings.py
+++ mysite/settings.py
@@ -144,15 +144,13 @@ CART_SESSION_ID = 'cart'
 # --------------------------
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT', '5432'),
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
+
 
 
 # --------------------------
