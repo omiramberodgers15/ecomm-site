54f9e6ebc34e0b14f1f0558e0b0580e77e647534
omirambe361 <omirambe361@gmail.com>
2025-12-12 11:53:58 +0200

    last push for railway update

--- mysite/settings.py
+++ mysite/settings.py
@@ -144,17 +144,15 @@ CART_SESSION_ID = 'cart'
 # --------------------------
 
 DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME':'railway',
-            'USER':'postgres',
-            'PASSWORD':os.getenv('DB_PASSWORD'),
-            'HOST':'nozomi.proxy.rlwy.net',
-            'PORT':'32031',
-        }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
-
-
+}
 
 
 # --------------------------
